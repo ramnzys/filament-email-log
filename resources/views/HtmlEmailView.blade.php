29c67e97cd8ac512ecd8b4d4bfe84c796d71768b
Ramón E. Zayas <ramnzys@gmail.com>
2022-06-16 10:41:09 -0500

    fix: Add iframe to email display to limit styles

--- resources/views/HtmlEmailView.blade.php
+++ resources/views/HtmlEmailView.blade.php
@@ -1,5 +1,5 @@
 <x-forms::field-wrapper :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()" :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
-    <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }">
-        {!! $getState() !!}
+    <div>
+        <iframe class="w-full h-screen" srcdoc=" {{$getState() }}" seamless frameborder="0"></iframe>
     </div>
 </x-forms::field-wrapper>
